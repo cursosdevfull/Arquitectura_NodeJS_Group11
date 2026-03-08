5ed7118eeceddb285de97d8efc4c10c51cf65946
Sergio Hidalgo <sergiohidalgocaceres@gmail.com>
2026-03-08 10:55:48 -0500

    feat: add configuration management with YAML support and update dependencies

--- courses/src/app.module.ts
+++ courses/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { configEnv } from './core/configuration/config-env';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configEnv],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ courses/src/core/configuration/config-env.ts
@@ -0,0 +1,24 @@
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { z } from 'zod';
+
+const CONFIG_PATH = join(__dirname, '../..', 'config.yml');
+
+const schema = z.object({
+  port: z.coerce.number(),
+  database: z.object({
+    host: z.string().min(3),
+    port: z.coerce.number(),
+    user: z.string().min(3),
+    password: z.string().min(8),
+    name: z.string().min(3),
+  }),
+});
+
+export const configEnv = () => {
+  const fileContents = readFileSync(CONFIG_PATH, 'utf8');
+
+  const config = yaml.load(fileContents);
+  return schema.parse(config);
+};
